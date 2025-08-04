5d6765b089ea6c401f100db356b9f125dc314c8d
Filipe Santiago Santos <filipecacule@gmail.com>
2025-08-03 23:37:01 -0300

    feat: update output_voice.py to use TTS API and improve audio playback; add conversation history to .gitignore

--- src/interfaces/output/output_voice.py
+++ src/interfaces/output/output_voice.py
@@ -1,15 +1,16 @@
-from gtts import gTTS
-import tempfile
-from playsound import playsound
-import os
+from TTS.api import TTS
+import sounddevice as sd
+
+import logging
+logging.getLogger("TTS").setLevel(logging.ERROR)
+
+tts_model = TTS(model_name="tts_models/pt/cv/vits", progress_bar=False, gpu=False)
+
 
 def fale(texto):
     try:
-        tts = gTTS(text=texto, lang='pt-br')
-        with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as f:
-            caminho_audio = f.name
-        tts.save(caminho_audio)
-        playsound(caminho_audio)
-        os.remove(caminho_audio)
+        audio = tts_model.tts(text=texto)
+        sd.play(audio, samplerate=tts_model.synthesizer.output_sample_rate)
+        sd.wait()
     except Exception as e:
-        print(f"[X] Falha ao falar: {e}")
+        print(f"[X] Falha ao falar com Coqui: {e}")
